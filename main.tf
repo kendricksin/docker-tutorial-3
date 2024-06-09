40c84024e833a7aabef817b731d5d3415be4c2b3
unknown <kendrick_sin@hotmail.com>
2024-06-09 13:46:45 +0800

    modify terraform file to fix bugs

--- main.tf
+++ main.tf
@@ -1,23 +1,29 @@
-# main.tf
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13"
+    }
+  }
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "ubuntu" {
-  name = "ubuntu:20.04"
+  name         = "ubuntu:20.04"
   keep_locally = false
 }
 
-resource "docker_container" "ubuntu" {
+resource "docker_container" "ubuntu_container" {
   name  = "ubuntu_container"
   image = docker_image.ubuntu.latest
 
-  # Expose port 22 for SSH (optional)
   ports {
     internal = 22
     external = 2222
   }
 
-  # Commands to keep the container running
   entrypoint = ["/bin/bash", "-c", "while true; do sleep 1000; done"]
 }
